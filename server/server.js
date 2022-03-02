7d96dda895c8bc09bc5a9109c16cb8b80e617891
AP <porwal.1@iitj.ac.in>
2022-03-02 22:44:55 +0530

    Trying to host backend only

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dbConnect();
 
 const express = require('express');
 const app = express();
-const path = require('path');
+// const path = require('path');
 
 // Custom Error Handler
 const { errHandler } = require('./middleware/errorMiddleware')
@@ -23,16 +23,16 @@ app.use('/api/users', require('./routes/userRoutes'));
 app.use(errHandler)
 
 //Serve static assets if application is in production
-if (process.env.NODE_ENV === "production") {
-    //Set Static Folder
-    app.use(express.static('../client/build'))
+// if (process.env.NODE_ENV === "production") {
+//     //Set Static Folder
+//     app.use(express.static('../client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     })
+// }
 
-console.log(path.resolve(__dirname, '..\client', 'build', 'index.html'))
+// console.log(path.resolve(__dirname, '..\client', 'build', 'index.html'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
