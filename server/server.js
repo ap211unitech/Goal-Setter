39f2549ee37db9b6d9127618cfb1d858313d8c3d
AP <porwal.1@iitj.ac.in>
2022-03-04 20:19:54 +0530

    Deployed on Netlify and Heroku Successfully

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ dbConnect();
 const express = require('express');
 const app = express();
 const cors = require("cors");
-// const path = require('path');
 
 // Custom Error Handler
 const { errHandler } = require('./middleware/errorMiddleware')
@@ -16,10 +15,7 @@ const { errHandler } = require('./middleware/errorMiddleware')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// var corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+
 app.use(cors());
 
 // Routes
@@ -28,18 +24,6 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 // Error Handler
 app.use(errHandler)
-
-//Serve static assets if application is in production
-// if (process.env.NODE_ENV === "production") {
-//     //Set Static Folder
-//     app.use(express.static('../client/build'))
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     })
-// }
-
-// console.log(path.resolve(__dirname, '..\client', 'build', 'index.html'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
